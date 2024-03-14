b263dcbe91b58175c5a206788a9dad0d73191618
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-03-13 23:28:02 -0500

    updated option to show more projects

--- js/script.js
+++ js/script.js
@@ -94,3 +94,21 @@ function scrollFunction() {
         document.getElementById("navbar").style.background = "none";
     }
 }
+
+// show more and show less
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.previousElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+      document.querySelector("#show-button").innerHTML = "Show More";
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      document.querySelector("#show-button").innerHTML = "Show Less";
+    }
+  });
+}
